aa9c1e529b9c677160d89740271cb5a317d560a6
msaenzg161 <msaenzg161@gmail.com>
2018-11-12 22:07:58 -0600

    Test

--- ProyectoFinal_C#/app/src/main/java/com/example/ulacit/proyectofinal/Login.java
+++ ProyectoFinal_C#/app/src/main/java/com/example/ulacit/proyectofinal/Login.java
@@ -16,7 +16,7 @@ public class Login extends Activity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        inicializaPantalla();
+        inicializaPantalla(); //test
     }
 
     @Override
